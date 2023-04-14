1dc97a81702248ce5e5c6404442abce6561f6363
iandoesallthethings <iandoesallthethings@gmail.com>
2023-04-14 16:36:02 -0400

    Fix types in analytics.

---
+++ src/lib/vitals.ts
@@ -0,0 +1,67 @@
+import type { Page } from '@sveltejs/kit'
+import type { Metric } from 'web-vitals'
+import { onCLS, onFCP, onFID, onLCP, onTTFB } from 'web-vitals'
+
+const vitalsUrl = 'https://vitals.vercel-analytics.com/v1/vitals'
+
+function getConnectionSpeed() {
+	return 'connection' in navigator &&
+		navigator['connection'] &&
+		'effectiveType' in navigator['connection']
+		? navigator['connection']['effectiveType']
+		: ''
+}
+
+export interface AnalyticsOptions {
+	path: Page['url']['pathname']
+	params: Page['params']
+	analyticsId: string
+	debug?: boolean
+}
+
+function sendToAnalytics(metric: Metric, options: AnalyticsOptions) {
+	const page = Object.entries(options.params).reduce(
+		(acc, [key, value]) => acc.replace(value, `[${key}]`),
+		options.path
+	)
+
+	const body = {
+		dsn: options.analyticsId, // qPgJqYH9LQX5o31Ormk8iWhCxZO
+		id: metric.id, // v2-1653884975443-1839479248192
+		page, // /blog/[slug]
+		href: location.href, // https://{my-example-app-name-here}/blog/my-test
+		event_name: metric.name, // TTFB
+		value: metric.value.toString(), // 60.20000000298023
+		speed: getConnectionSpeed(), // 4g
+	} as unknown as URLSearchParams
+
+	if (options.debug) {
+		console.log('[Analytics]', metric.name, JSON.stringify(body, null, 2))
+	}
+
+	const blob = new Blob([new URLSearchParams(body).toString()], {
+		// This content type is necessary for `sendBeacon`
+		type: 'application/x-www-form-urlencoded',
+	})
+	if (navigator.sendBeacon) {
+		navigator.sendBeacon(vitalsUrl, blob)
+	} else
+		fetch(vitalsUrl, {
+			body: blob,
+			method: 'POST',
+			credentials: 'omit',
+			keepalive: true,
+		})
+}
+
+export function webVitals(options: AnalyticsOptions) {
+	try {
+		onFID((metric) => sendToAnalytics(metric, options))
+		onTTFB((metric) => sendToAnalytics(metric, options))
+		onLCP((metric) => sendToAnalytics(metric, options))
+		onCLS((metric) => sendToAnalytics(metric, options))
+		onFCP((metric) => sendToAnalytics(metric, options))
+	} catch (err) {
+		console.error('[Analytics]', err)
+	}
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,26 @@
 	import '../app.css'
 	import 'highlight.js/styles/github.css'
 	import { dev } from '$app/environment'
-	import { inject } from '@vercel/analytics'
+	import { inject as VercelAnalytics } from '@vercel/analytics'
+	import { webVitals as VercelWebVitals } from '$lib/vitals'
 	import Header from '$components/Header.svelte'
 	import Pool from '$ukiyo/Pool.svelte'
 	import Konami from '$components/Konami.svelte'
 	import Footer from '$components/Footer.svelte'
+	import { browser } from '$app/environment'
+	import { page } from '$app/stores'
 
-	inject({ mode: dev ? 'development' : 'production' })
+	VercelAnalytics({ mode: dev ? 'development' : 'production' })
+
+	let analyticsId: string = import.meta.env.VERCEL_ANALYTICS_ID
+
+	$: if (browser && analyticsId) {
+		VercelWebVitals({
+			path: $page.url.pathname,
+			params: $page.params,
+			analyticsId,
+		})
+	}
 </script>
 
 <theme>
