9a7b33b25dcacb45a1c1a02dcdd2451acbfdfb57
iandoesallthethings <iandoesallthethings@gmail.com>
2023-04-13 22:34:36 -0400

    Fix Page.svelte.

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -14,16 +14,17 @@
 	onMount(() => floatToTop(page))
 </script>
 
+<div class="overlay" />
 <div bind:this={page} class="wrapper">
 	<article use:clickOutside={close} transition:fade class="page">
 		<slot />
 	</article>
 </div>
 
-<style>
-	/* .overlay {
+<style lang="postcss">
+	.overlay {
 		@apply absolute top-0 left-0 w-full h-full flex justify-center items-center z-30 cursor-pointer;
-	} */
+	}
 
 	.wrapper {
 		@apply absolute top-0 left-0 w-full h-full flex items-center justify-center py-32;
