276cafb610bf0ad22d1fa842adf940056630f404
iandoesallthethings <iandoesallthethings@gmail.com>
2023-04-24 13:24:01 -0400

    Remove the query param thing.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,13 +1,13 @@
-import type { FieldName } from '$types'
-import focus from '$lib/focus'
+// import type { FieldName } from '$types'
+// import focus from '$lib/focus'
 
 export async function load({ data, url }) {
-	const focusedFieldName = url.searchParams.get('f') as FieldName | null
+	// const focusedFieldName = url.searchParams.get('f') as FieldName | null
 
-	const preferredFocus = focusedFieldName ?? 'all the things'
+	// const preferredFocus = focusedFieldName ?? 'all the things'
 
-	focus.set(preferredFocus)
-	url.searchParams.delete('f')
+	// focus.set(preferredFocus)
+	// url.searchParams.delete('f')
 
 	return data
 }
