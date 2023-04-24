2e0f4dd036ef66b8995d86de8193475c3cd98291
iandoesallthethings <iandoesallthethings@gmail.com>
2023-04-24 13:30:27 -0400

    Re-add query param in a way that doesn't break the page.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,13 +1,12 @@
-// import type { FieldName } from '$types'
-// import focus from '$lib/focus'
+import type { FieldName } from '$types'
+import focus from '$lib/focus'
 
 export async function load({ data, url }) {
-	// const focusedFieldName = url.searchParams.get('f') as FieldName | null
+	const fieldNameToFocus = url.searchParams.get('f') as FieldName | null
 
-	// const preferredFocus = focusedFieldName ?? 'all the things'
-
-	// focus.set(preferredFocus)
-	// url.searchParams.delete('f')
+	if (fieldNameToFocus) {
+		focus.set(fieldNameToFocus)
+	}
 
 	return data
 }
