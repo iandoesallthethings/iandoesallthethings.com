eccf2f2732b9f29eb2ec692b8acf42698255b3e6
iandoesallthethings <iandoesallthethings@gmail.com>
2023-08-15 16:20:14 -0400

    Improve button spacing.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -26,10 +26,10 @@
 
 <style lang="postcss">
 	footer {
-		@apply absolute bottom-0 flex justify-between px-4 py-2 w-min items-center;
+		@apply absolute bottom-0 flex justify-between p-2 w-min items-center;
 	}
 
 	a {
-		@apply cursor-pointer;
+		@apply cursor-pointer p-1;
 	}
 </style>
