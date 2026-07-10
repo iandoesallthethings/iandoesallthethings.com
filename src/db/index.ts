3dbde8fefaec8ffb4a0d5c86130db712d30e20e6
iandoesallthethings <iandoesallthethings@gmail.com>
2026-07-10 16:23:40 -0400

    Delete Notion, S3, and Postgres code.

--- src/db/index.ts
+++
@@ -1,7 +0,0 @@
-// import { PrismaClient } from '@prisma/client'
-
-// export const prisma = new PrismaClient()
-
-export * as Notion from '$db/Notion'
-export * as Fields from '$db/Fields'
-export * as Projects from '$db/Projects'
--- src/types.ts
+++ src/types.ts
@@ -1,28 +1,20 @@
-export type { QueryDatabaseParameters } from '@notionhq/client/build/src/api-endpoints'
-
 export type UrlString = string
-
 export type HtmlString = string
-export type PlainText = string
-export type CSSClasses = string
 
 export type FieldName = string
 
 export type Row = Field | Project
 
-export interface BaseRow {
+export interface Field {
 	id: string
-	properties?: Record<PropertyName, Property>
-}
-
-export interface Field extends BaseRow {
 	name: FieldName
 	blurb?: string
 	order: number
 	published: boolean
 }
 
-export interface Project extends BaseRow {
+export interface Project {
+	id: string
 	name: string
 	subtitle: string
 	route: string
@@ -34,108 +26,9 @@ export interface Project extends BaseRow {
 	indev: boolean
 }
 
-export type PropertyName = 'title' | 'rich_text' | 'checkbox' | 'url' | 'multi_select' | 'files'
-
-export type Property =
-	| TitleProperty
-	| RichTextProperty
-	| CheckboxProperty
-	| UrlProperty
-	| MultiSelectProperty
-	| FilesProperty
-
-export interface BaseProperty {
-	id: string
-	type: PropertyName
-}
-
-export interface TitleProperty extends BaseProperty {
-	title: { plain_text: string }[]
-}
-
-export interface RichTextProperty extends BaseProperty {
-	rich_text: RichTextChunk[]
-}
-
-export interface RichTextChunk {
-	type: string
-	plain_text: string
-	text: { content: string }
-	href?: string
-	annotations: {
-		bold?: boolean
-		italic?: boolean
-		strikethrough?: boolean
-		underline?: boolean
-		code?: boolean
-		color?: string
-	}
-}
-
-export interface CheckboxProperty extends BaseProperty {
-	checkbox: { checkbox: boolean }
-}
-
-export interface UrlProperty extends BaseProperty {
-	url: { url: UrlString }
-}
-
-export interface MultiSelectProperty extends BaseProperty {
-	multi_select: { name: string }[]
-}
-
-export interface FilesProperty extends BaseProperty {
-	files: { file: { url: UrlString } }[]
-}
-
 export type Page<T extends Row> = T & {
 	page: HtmlString
 }
-
-export type BlockType =
-	| 'heading_1'
-	| 'heading_2'
-	| 'heading_3'
-	| 'paragraph'
-	| 'numbered_list_item'
-	| 'bulleted_list_item'
-	| 'divider'
-	| 'image'
-	| 'to_do'
-	| 'callout'
-	| 'code'
-	| 'video'
-	| 'embed'
-	| 'table'
-	| 'fallback'
-
-export interface Block {
-	type: BlockType
-	rich_text: RichTextChunk[]
-	text: RichTextChunk[]
-	caption: RichTextChunk[]
-	checked: boolean
-	file: { url: string }
-	icon: { emoji: string }
-	language: string
-	external: { url: string }
-}
-
-export interface Db {
-	fields: Field[]
-	projects: Project[]
-}
-
-export interface ProjectCache {
-	[key: string]: Project
-}
-
-export interface Props {
-	props: JsonObject
-}
-
-// Recursive type for value that can be encoded.
-export type JsonValue = string | number | boolean | null | JsonValue[] | JsonObject
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ArbitraryFunction = (...args: any[]) => unknown
@@ -143,13 +36,4 @@ export type ArbitraryFunction = (...args: any[]) => unknown
 export interface ArbitraryClass<T> {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	new (...args: any[]): T
-}
-
-export interface JsonObject {
-	[key: string]: JsonValue
-}
-
-export interface NotionSort {
-	property: string
-	direction: 'ascending' | 'descending'
 }
